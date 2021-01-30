5ae2eb76d2b57e417854dd2f765f3d3ffa2f87d0
Yener Yardim <yyardim@gmail.com>
2021-01-30 03:22:56 -0800

    simple listener/client

---
+++ UniqNumsServer/NumsClient/NumsClient/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumsClient
+{
+    // Client app is sending messages to a Server/Listener
+    // Both listener & client can send messages back & forth once a communication is established
+    public class NumsClient
+    {
+        public static int Main(string[] args)
+        {
+            StartClient();
+            return 0;
+        }
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[18];
+
+            try
+            {
+                // Connect to a Remote server
+                // Get Host IP Address that is used to establish a connection
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 4000);
+
+                // Create a TCP/IP socket
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint
+                try
+                {
+                    // Connect to Remote Endpoint
+                    sender.Connect(remoteEndPoint);
+
+                    Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array
+                    byte[] msg = Encoding.ASCII.GetBytes("123456789" + Environment.NewLine); //replace <EOF> with proper eof
+
+                    // Send the data through the socket
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (ArgumentNullException argNullExc)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", argNullExc.ToString());
+                }
+                catch(SocketException sockExc)
+                {
+                    Console.WriteLine("SocketException: {0}", sockExc.ToString());
+                }
+                catch(Exception exc)
+                {
+                    Console.WriteLine("Unexpected exception: {0}", exc.ToString());
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
+        }
+    }
+}
---
+++ UniqNumsServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqNumsServer
+{
+    public class NumberListener
+    {
+        public static int Main(string[] args)
+        {
+            StartServer();
+            return 0;
+        }
+
+        public static void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 4000);
+
+            try
+            {
+                // Create a Socket that will use TCP protocol
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // A Socket must be associated with an endpoint using the Bind method
+                listener.Bind(localEndPoint);
+                // Specify how many requests a Socket can listen before it gives Server busy response
+                listener.Listen(5);
+
+                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+
+                // Incoming data from the client
+                string data = null; ///TODO Use stringbuilder instead
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    // Environment.NewLine, which is \r\n is 2 bytes. Decimal is 16  bytes. So we expect 18 bytes.
+                    bytes = new byte[18];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf(Environment.NewLine) > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received: {0}", data);
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg); ///We aren't sending anything to client
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
