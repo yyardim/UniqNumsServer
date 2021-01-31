9d23692aee661efbf1eb09f7c97e4a94537836a9
Yener Yardim <yyardim@gmail.com>
2021-01-30 19:25:11 -0800

    add nums txt file to be read in client

--- UniqNumsServer/NumsClient/NumsClient.cs
+++ UniqNumsServer/NumsClient/NumsClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -42,8 +43,10 @@ namespace NumsClient
                 client.BeginConnect(remoteEndPoint, new AsyncCallback(ConnectCallback), client);
                 connectDone.WaitOne();
 
+                string nums = ReadNums();
+
                 // Send data to the remote device
-                Send(client, "123456789" + Environment.NewLine);
+                Send(client, nums);
                 sendDone.WaitOne();
 
                 // Release the socket
@@ -53,61 +56,25 @@ namespace NumsClient
             } catch (Exception exc) {
                 Console.WriteLine(exc.ToString());
             }
+        }
 
-            #region
-            //byte[] bytes = new byte[18];
+        /// <summary>
+        /// Reads numbers from "nums.txt" file that should be placed in the same directory as the client application
+        /// The assumption of the file is similar to the server requirements, where each number is in its own line
+        /// Skipping any error validations here as this client app is not the target but it's merely here to send numbers to the listener
+        /// </summary>
+        /// <returns>string</returns>
+        private static string ReadNums() {
+            var numsFile = Path.Combine(Directory.GetCurrentDirectory(), "nums.txt");
+            var sbNums = new StringBuilder();
+            if (File.Exists(numsFile)) {
+                string[] lines = File.ReadAllLines(numsFile);
+                foreach (var line in lines) {
+                    sbNums.Append(line + Environment.NewLine);
+                }
+            }
 
-            //try
-            //{
-            //    // Connect to a Remote server
-            //    // Get Host IP Address that is used to establish a connection
-            //    IPHostEntry host = Dns.GetHostEntry("localhost");
-            //    IPAddress ipAddress = host.AddressList[0];
-            //    IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 4000);
-
-            //    // Create a TCP/IP socket
-            //    Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            //    // Connect the socket to the remote endpoint
-            //    try
-            //    {
-            //        // Connect to Remote Endpoint
-            //        sender.Connect(remoteEndPoint);
-
-            //        Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
-
-            //        // Encode the data string into a byte array
-            //        byte[] msg = Encoding.ASCII.GetBytes("123456789" + Environment.NewLine); //replace <EOF> with proper eof
-
-            //        // Send the data through the socket
-            //        int bytesSent = sender.Send(msg);
-
-            //        // Receive the response from the remote device
-            //        int bytesRec = sender.Receive(bytes);
-            //        Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-            //        // Release the socket
-            //        sender.Shutdown(SocketShutdown.Both);
-            //        sender.Close();
-            //    }
-            //    catch (ArgumentNullException argNullExc)
-            //    {
-            //        Console.WriteLine("ArgumentNullException: {0}", argNullExc.ToString());
-            //    }
-            //    catch(SocketException sockExc)
-            //    {
-            //        Console.WriteLine("SocketException: {0}", sockExc.ToString());
-            //    }
-            //    catch(Exception exc)
-            //    {
-            //        Console.WriteLine("Unexpected exception: {0}", exc.ToString());
-            //    }
-            //}
-            //catch (Exception exc)
-            //{
-            //    Console.WriteLine(exc.ToString());
-            //}
-            #endregion
+            return sbNums.ToString();
         }
 
         private static void ConnectCallback(IAsyncResult asyncResult) {
--- UniqNumsServer/NumsSocketListener.cs
+++ UniqNumsServer/NumsSocketListener.cs
@@ -116,55 +116,5 @@ namespace UniqNumsServer
                 }
             }
         }
-
-        public static void StartServer()
-        {
-            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = host.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 4000);
-
-            try
-            {
-                // Create a Socket that will use TCP protocol
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                // A Socket must be associated with an endpoint using the Bind method
-                listener.Bind(localEndPoint);
-                // Specify how many requests a Socket can listen before it gives Server busy response
-                listener.Listen(5);
-
-                Console.WriteLine("Waiting for a connection...");
-                Socket handler = listener.Accept();
-
-                // Incoming data from the client
-                string data = null; ///TODO Use stringbuilder instead
-                byte[] bytes = null;
-
-                while (true)
-                {
-                    // Environment.NewLine, which is \r\n is 2 bytes. Decimal is 16  bytes. So we expect 18 bytes.
-                    bytes = new byte[1800];
-                    int bytesRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    if (data.IndexOf(Environment.NewLine) > -1)
-                    {
-                        break;
-                    }
-                }
-
-                Console.WriteLine("Text received: {0}", data);
-
-                byte[] msg = Encoding.ASCII.GetBytes(data);
-                handler.Send(msg); ///We aren't sending anything to client
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            Console.WriteLine("\n Press any key to continue");
-            Console.ReadLine();
-        }
     }
 }
